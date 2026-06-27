876c2c865d9aa46cde96a0722532233742c8f90c
Mario Pavlov <marioppavlov@gmail.com>
2026-06-27 16:20:28 +0300

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+import java.util.List;
 import java.util.Scanner;
 
 public class Main {
@@ -7,15 +9,33 @@ public class Main {
             System.out.print("$ ");
 
             Scanner scanner = new Scanner(System.in);
-            var command = scanner.nextLine();
 
-            if (command.equals("exit")) {
-                return;
+            if (!scanner.hasNext()) {
+                exit();
             }
 
-            System.out.printf("%s: command not found\n", command);
+            var input = scanner.nextLine().split(" ");
+            var command = input[0];
+            var rest = Arrays.copyOfRange(input, 1, input.length);
+
+            switch (command) {
+                case "exit":
+                    exit();
+                case "echo":
+                    echo(rest);
+                    break;
+                default:
+                    System.out.printf("%s: command not found\n", command);
+                    break;
+            }
         }
+    }
 
+    public static void exit() {
+        System.exit(0);
+    }
 
+    public static void echo(String... args) {
+        System.out.println(String.join(" ", args));
     }
 }
