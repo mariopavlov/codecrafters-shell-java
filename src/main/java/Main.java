ea1dc51b3c02172a75d08059e6deec6a35c543ea
Mario Pavlov <marioppavlov@gmail.com>
2026-06-27 16:01:48 +0300

    codecrafters submit [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -8,11 +8,12 @@ public class Main {
 
             Scanner scanner = new Scanner(System.in);
             var command = scanner.nextLine();
-            System.out.printf("%s: command not found\n", command);
 
             if (command.equals("exit")) {
                 return;
             }
+
+            System.out.printf("%s: command not found\n", command);
         }
 
 
